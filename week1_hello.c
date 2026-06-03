96e3e6b21e49bbe2ad7c4db45e656af6468943cb
baek-study <haejung_baek@naver.com>
2026-06-04 04:30:51 +0900

    Add files via upload

---
+++ week1_hello.c
@@ -0,0 +1,15 @@
+/* 
+	강  좌: 제 1 강 
+  파일명: week1_hello.c
+	설  명: 무조건 따라하는 hello world 출력! 
+	작성자: 마루
+*/
+
+#include<stdio.h>    // 전처리
+
+int main()          // 메인 함수
+{
+	//hello, world 화면 출력 
+	printf("Hello, World\n");
+	return 0;
+}
